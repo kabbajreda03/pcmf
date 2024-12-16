abcbb65f9407720a0033c4e2442a409ace198961
Ismail Badaoui <ismail.badaoui@grenoble-inp.org>
2024-12-17 00:25:29 +0100

    Add Option Payoffs

--- PricerEngine/Options/ConditionalBasketOption.cpp
+++ PricerEngine/Options/ConditionalBasketOption.cpp
@@ -4,5 +4,32 @@ using namespace options;
 
 double ConditionalBasketOption::get_payoff(const PnlMat * const underlying_paths) const
 {
-	return 0.0;
+	double total_payoff = 0.0;
+    int n_monitoring_dates = parameters.monitoringDates->size;
+    int n_assets = underlying_paths->n;
+    double dt = GET(parameters.monitoringDates, n_monitoring_dates - 1) / (underlying_paths->m - 1);
+    
+    bool previous_payoffs_zero = true;
+    
+    for (int m = 0; m < n_monitoring_dates && previous_payoffs_zero; m++) {
+        int date_index = (int)(GET(parameters.monitoringDates, m) / dt);
+            
+        double basket_value = 0.0;
+        for (int n = 0; n < n_assets; n++) {
+            basket_value += MGET(underlying_paths, date_index, n);
+        }
+        basket_value /= n_assets;
+            
+        double current_strike = GET(parameters.strikes, m);
+            
+        double current_payoff = std::max(basket_value - current_strike, 0.0);
+            
+        total_payoff += current_payoff;
+            
+        if (current_payoff > 0.0) {
+            previous_payoffs_zero = false;
+        }
+    }
+    
+    return total_payoff;
 }
--- PricerEngine/Options/ConditionalMaxOption.cpp
+++ PricerEngine/Options/ConditionalMaxOption.cpp
@@ -4,5 +4,31 @@ using namespace options;
 
 double ConditionalMaxOption::get_payoff(const PnlMat * const underlying_paths) const
 {
-	return 0.0;
+	double total_payoff = 0.0;
+    int n_monitoring_dates = parameters.monitoringDates->size;
+    int n_assets = underlying_paths->n;
+    double dt = GET(parameters.monitoringDates, n_monitoring_dates - 1) / (underlying_paths->m - 1);
+    
+    bool previous_payoff_zero = true; // Track if previous payoff was 0
+    
+    for (int m = 0; m < n_monitoring_dates; m++) {
+        
+        int date_index = (int)(GET(parameters.monitoringDates, m) / dt);
+            
+        double max_value = MGET(underlying_paths, date_index, 0);
+        for (int n = 1; n < n_assets; n++) {
+            max_value = std::max(max_value, MGET(underlying_paths, date_index, n));
+        }
+            
+        double current_strike = GET(parameters.strikes, m);
+            
+        double current_payoff = std::max(max_value - current_strike, 0.0);
+        if (previous_payoff_zero) {    
+            total_payoff += current_payoff;
+		}
+		
+        previous_payoff_zero = (current_payoff == 0.0);
+    }
+    
+    return total_payoff;
 }
